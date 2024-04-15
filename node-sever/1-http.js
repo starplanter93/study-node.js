296ac4d167fc556b06e02c4b3a4e66ebce2b8424
starplanter93 <starplanter93@gmail.com>
2024-04-15 20:38:02 +0900

    html형식 보내기

--- node-sever/1-http.js
+++ node-sever/1-http.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.write('welcome');
-  res.end();
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    console.log(process.cwd());
+    fs.createReadStream('./html/index.html').pipe(res);
+  } else if (url === '/courses') {
+    fs.createReadStream('./html/courses.html').pipe(res);
+  } else {
+    fs.createReadStream('./html/not-found.html').pipe(res);
+  }
 });
 
 server.listen(8080);
